390d043aafd960c2e0befb9f6a908eaa87ae0f2e
Masataka Anime <Judas3991@gmail.com>
2020-10-20 22:11:35 -0500

    finish expander view successfull

---
+++ Expander/Expander/Model/Agenda.cs
@@ -0,0 +1,18 @@
+﻿namespace Expander.Model
+{
+    using System;
+    using System.Collections.ObjectModel;
+
+    public class Agenda
+    {
+        public string Topic { get; set; }
+
+        public string Duration { get; set; }
+
+        public string Color { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public ObservableCollection<Speaker> Speakers { get; set; }
+    }
+}
