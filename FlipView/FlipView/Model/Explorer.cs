5d9f39242de96372093617942d1d4248724a0f37
Masataka Anime <Judas3991@gmail.com>
2020-10-21 22:30:48 -0500

    finish proyect

---
+++ FlipView/FlipView/Model/Explorer.cs
@@ -0,0 +1,13 @@
+﻿namespace FlipView.Model
+{
+    public class Explorer
+    {
+        public string Image { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Caption { get; set; }
+    }
+}
