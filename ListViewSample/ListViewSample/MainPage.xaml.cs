be6e7b645eae0fe26552d69631a36030fc9883f4
Masataka Anime <Judas3991@gmail.com>
2020-10-28 21:18:45 -0500

    finish project

--- ListViewSample/ListViewSample/MainPage.xaml.cs
+++ ListViewSample/ListViewSample/MainPage.xaml.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
-
-namespace ListViewSample
+﻿namespace ListViewSample
 {
+    using ListViewSample.ViewModel;
+    using Xamarin.Forms;
+
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new MainViewModel();
         }
     }
 }
